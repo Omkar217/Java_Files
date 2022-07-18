1558552b20d85ef4dedf7f1b592c22fec496819d
Omkar217 <60723310+Omkar217@users.noreply.github.com>
2022-07-18 21:31:02 +0530

    Add files via upload

---
+++ JavaApp1.java
@@ -0,0 +1,28 @@
+
+public class JavaApp1 {
+	boolean height;//primitive datatypes.
+	double  age;
+	int     school;
+	float   student;
+	long    occupation;
+	short   colour;
+	
+	public JavaApp1() {
+		System.out.println("Constructor called");
+		
+	}
+ public static void main(String args[])
+	{
+	 JavaApp1 v;
+	 v=new JavaApp1();
+	System.out.println(v.age);
+	System.out.println(v.height);
+	System.out.println(v.school);
+	System.out.println(v.student);
+	System.out.println(v.occupation);
+	System.out.println(v.colour);
+
+	
+	}
+
+}
